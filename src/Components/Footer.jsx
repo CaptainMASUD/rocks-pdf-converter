300caa177c9b1205b6f15a33c9beaa608609a8ba
masudul alam <masudulalam972@gmail.com>
2024-04-02 18:15:07 +0600

    version-1.1.4

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gradient-to-r from-purple-500 to-pink-500 text-white py-4">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-center"> {/* Centering the text */}
-          <p>© 2024 Your Company. All rights reserved.</p>
+          <p>© 2024 RHM PDF Converter. All rights reserved.</p>
         </div>
       </div>
     </footer>
